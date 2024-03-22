d53011725699f93a2ce981a385e3b8bb63350231
Asrom11 <asrom.11@mail.ru>
2024-03-22 22:43:43 +0500

    Add Pool for broker connections

--- Libs/Core/BrokerLogic/BrokerStartUp.cs
+++ Libs/Core/BrokerLogic/BrokerStartUp.cs
@@ -4,6 +4,8 @@ using ExampleCore.HttpLogic.Services;
 using ExampleCore.HttpLogic.Services.Interfaces;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.DependencyInjection.Extensions;
+using Microsoft.Extensions.ObjectPool;
+using RabbitMQ.Client;
 
 namespace ExampleCore.BrokerLogic;
 
@@ -12,6 +14,16 @@ public static class HttpServiceStartup
 
     public static IServiceCollection AddBrokerLogic(this IServiceCollection services)
     {
+        services.TryAddSingleton<IPooledObjectPolicy<IConnection>>(_ =>
+        {
+            return new ConnectionPooledPolicy("localhost");
+        });
+        
+        services.TryAddSingleton<ObjectPool<IConnection>>(serviceProvider =>
+        {
+            var policy = serviceProvider.GetRequiredService<IPooledObjectPolicy<IConnection>>();
+            return new DefaultObjectPool<IConnection>(policy);
+        });
         services.TryAddKeyedTransient<IHttpRequestService, ProducerRequestService>("broker");
         return services;
     }
--- Libs/Core/BrokerLogic/Producer/Services/ProducerRequestService.cs
+++ Libs/Core/BrokerLogic/Producer/Services/ProducerRequestService.cs
@@ -8,6 +8,7 @@ using MassTransit;
 using Medo;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.ObjectPool;
 using Newtonsoft.Json;
 using RabbitMQ.Client;
 using RabbitMQ.Client.Events;
@@ -16,33 +17,24 @@ namespace ExampleCore.BrokerLogic.Services;
 
 public class ProducerRequestService: IHttpRequestService
 {
-    
-    private readonly IConnection connection;
-    private readonly IModel channel;
     private readonly string replyQueueName;
     
     private readonly IEnumerable<ITraceWriter> _traceWriterList;
-    //private readonly IClientFactory _clientFactory;
-    private readonly ConcurrentDictionary<string, TaskCompletionSource<string>> callbackMapper = new();
-    public ProducerRequestService( IEnumerable<ITraceWriter> traceWriterList)
+    private readonly ConcurrentDictionary<string, TaskCompletionSource<string>> _callbackMapper = new();
+    private readonly ObjectPool<IConnection> _connectionPool;
+    public ProducerRequestService( IEnumerable<ITraceWriter> traceWriterList, ObjectPool<IConnection> connectionPool)
     {
-    //    _clientFactory = clientFactory;
+        _connectionPool = connectionPool;
         _traceWriterList = traceWriterList;
-        var factory = new ConnectionFactory 
-        {        
-            HostName = "localhost",
-            Password = "guest",
-            UserName = "guest" 
-        };
-
-        connection = factory.CreateConnection();
-        channel = connection.CreateModel();
+        
+        var connection = _connectionPool.Get();
+        var channel = connection.CreateModel();
         
         replyQueueName = channel.QueueDeclare().QueueName;
         var consumer = new EventingBasicConsumer(channel);
         consumer.Received += (model, ea) =>
         {
-            if (!callbackMapper.TryRemove(ea.BasicProperties.CorrelationId, out var tcs))
+            if (!_callbackMapper.TryRemove(ea.BasicProperties.CorrelationId, out var tcs))
                 return;
             var body = ea.Body.ToArray();
             var response = Encoding.UTF8.GetString(body);
@@ -53,46 +45,62 @@ public class ProducerRequestService: IHttpRequestService
             queue: replyQueueName,
             autoAck: true);
         
+        _connectionPool.Return(connection);
     }
 
     public async Task<HttpResponse<TResponse>> SendRequestAsync<TResponse, TRequest>(HttpRequestData requestData,
         HttpConnectionData connectionData = default) where TRequest : class
-        where TResponse : class
+        where TResponse : class 
     {
+        var connection = _connectionPool.Get();
+        using var channel = connection.CreateModel();
+        var props = channel.CreateBasicProperties();
+        var correlationId = new Uuid7().ToString(); 
         
-       var props = channel.CreateBasicProperties();
-       var correlationId = new Uuid7().ToString();
+        props.CorrelationId = correlationId; 
+        props.ReplyTo = replyQueueName;
+        
+        foreach (var traceWriterList in _traceWriterList)
+        {
+            props.Headers.Add(traceWriterList.Name,traceWriterList.GetValue());
+        }
 
-       props.CorrelationId = correlationId;
-       props.ReplyTo = replyQueueName;
 
+        try
+        {
+            var res = await SendMessegeAsync<TResponse>(requestData, connectionData, correlationId, channel, props);
+            return new HttpResponse<TResponse>()
+            {
+                Body = res,
+                StatusCode = HttpStatusCode.OK
+            };
+        }
+           
+        finally
+        {
+            _connectionPool.Return(connection);
+        }
+    }
 
-       foreach (var traceWriterList in _traceWriterList)
-       {
-           props.Headers.Add(traceWriterList.Name,traceWriterList.GetValue());
-       }
-
-       var json = JsonConvert.SerializeObject(requestData.Body);
-       var messegeBytes = Encoding.UTF8.GetBytes(json);
+    private async Task<TResponse> SendMessegeAsync<TResponse>(HttpRequestData requestData, HttpConnectionData connectionData,
+        string correlationId, IModel channel, IBasicProperties props)
+    {
+        var json = JsonConvert.SerializeObject(requestData.Body);
+        var messegeBytes = Encoding.UTF8.GetBytes(json);
        
        
-       var tcs = new TaskCompletionSource<string>();
-       callbackMapper.TryAdd(correlationId, tcs);
+        var tcs = new TaskCompletionSource<string>();
+        _callbackMapper.TryAdd(correlationId, tcs);
        
-       channel.BasicPublish(exchange: String.Empty,
-           routingKey: "rpc/"+requestData.Uri,
-           basicProperties: props,
-           body: messegeBytes);
+        channel.BasicPublish(exchange: String.Empty,
+            routingKey: "rpc/"+requestData.Uri,
+            basicProperties: props,
+            body: messegeBytes);
 
-       connectionData.CancellationToken.Register(() => callbackMapper.TryRemove(correlationId, out _));
+        connectionData.CancellationToken.Register(() => _callbackMapper.TryRemove(correlationId, out _));
 
-       var ans = await tcs.Task;
-       var res = JsonConvert.DeserializeObject<TResponse>(ans);
-
-       return new HttpResponse<TResponse>()
-       {
-           Body = res,
-           StatusCode = HttpStatusCode.OK
-       };
+        var ans = await tcs.Task;
+        var res = JsonConvert.DeserializeObject<TResponse>(ans);
+        return res;
     }
 }
