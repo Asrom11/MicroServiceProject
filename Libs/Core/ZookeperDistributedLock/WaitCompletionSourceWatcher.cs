1a82fb16b051ea0ce58b1784a66de1ac3f115769
Asrom11 <asrom.11@mail.ru>
2024-04-27 23:10:11 +0500

    Add ZooKeeperDistributedSemaphore

---
+++ Libs/Core/ZookeperDistributedLock/WaitCompletionSourceWatcher.cs
@@ -0,0 +1,23 @@
+﻿using org.apache.zookeeper;
+
+namespace ExampleCore.ZookeperDistributedLock;
+
+public class WaitCompletionSourceWatcher : Watcher
+{
+    private readonly TaskCompletionSource<bool> _waitCompletionSource;
+
+    public WaitCompletionSourceWatcher(TaskCompletionSource<bool> waitCompletionSource)
+    {
+        _waitCompletionSource = waitCompletionSource;
+    }
+
+    public override Task process(WatchedEvent @event)
+    {
+        if (@event.getState() == Event.KeeperState.SyncConnected)
+        {
+            _waitCompletionSource.TrySetResult(true);
+        }
+
+        return Task.CompletedTask;
+    }
+}
